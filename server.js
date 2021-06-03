91c53ed9cae1d0db45f54e54880a67d7527c09c6
Hakan <hakanonize35@gmail.com>
2021-06-03 16:14:14 +0300

    Changed server codes

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-const express = require('express');
-const https = require("https"),
-  fs = require("fs");
+'use strict';
 
-const options = {
-  key: fs.readFileSync("/srv/www/keys/my-site-key.pem"),
-  cert: fs.readFileSync("/srv/www/keys/chain.pem")
-};
+// We’re setting up an extremely simple server here.
+const http = require('http');
 
-const app = express();
+// These could (should) be set as env vars.
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || 'localhost';
 
-app.use((req, res) => {
-  res.writeHead(200);
-  res.end("hello world\n");
-});
+// No matter what hits the server, we send the same thing.
+http.createServer((req, res) => {
 
-app.listen(8000);
+  // Tell the browser what’s coming.
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8',
+  });
 
-https.createServer(options, app).listen(8080);
+  // Send a simple message in HTML.
+  res.write('<h1>I’m a Node app!</h1>');
+  res.write('<p>And I’m <em>sooooo</em> secure.</p>');
+  res.end();
+}).listen(port, host);
+
+// This message prints in the console when the app starts.
+console.log(`App running at http://${host}:${port}`);
