e235e90cac4961b3b2a724f08e3565a0f2f3c88f
Hakan <hakanonize35@gmail.com>
2021-06-07 11:12:50 +0300

    Express and ejs added

--- server.js
+++ server.js
@@ -1,30 +1,17 @@
-'use strict';
+const express = require('express');
+const app = express();
 
-// We’re setting up an extremely simple server here.
-const http = require('http');
+app.set("view engine", "ejs");
+app.use("/public", express.static("public"));
 
-// These could (should) be set as env vars.
-const port = process.env.PORT || 5000;
-const host = process.env.HOST || 'localhost';
 
-// No matter what hits the server, we send the same thing.
-http.createServer((req, res) => {
+app.get('/', function(req,res) {
+  res.render('index')
+})
 
-  // Tell the browser what’s coming.
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8',
-  });
+const PORT = process.env.PORT || 5000;
 
-  // Send a simple message in HTML.
-  res.write('<h1>I’m a Node app!</h1>');
-  res.write('<p>And I’m <em>sooooo</em> secure.</p>');
-  res.write(`<img src='./public/1.jpg'/> `)
-  res.write(`<img src='./public/2.jpg'/> `)
-  res.write(`<img src='./public/3.jpg'/> `)
-  res.write(`<img src='./public/4.jpg'/> `)
-  res.write(`<img src='./public/5.jpg'/> `)
-  res.end();
-}).listen(port, host);
-
-// This message prints in the console when the app starts.
-console.log(`App running at http://${host}:${port}`);
+app.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Server is running on ${PORT}`);
+})
